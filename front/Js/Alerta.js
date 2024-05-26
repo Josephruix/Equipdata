cba6f271f98d675f9ed67d87be6ff6da6ed5822a
Josephruix <josephruix24@gmai.com>
2024-05-26 13:00:09 -0500

    No he podido hacer lo de los roles

--- front/Js/Alerta.js
+++
@@ -1,6 +0,0 @@
-function actualizarCampanaNotificaciones(mensaje) {
-    const campanaNotificaciones = document.getElementById('notificacion');
-    const nuevoMensaje = document.createElement('div');
-    nuevoMensaje.textContent = mensaje;
-    campanaNotificaciones.appendChild(nuevoMensaje);
-}
